d8b6162be618e3d65f6453855e395ed547fe93dd
Robert Wang <wangrob@amazon.com>
2023-09-08 06:18:15 +0000

    fix install docker commands

--- pcluster/containers/container-install.sh
+++ pcluster/containers/container-install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Install Docker and add user "ubuntu" to group "docker"
-sudo apt-get update && sudo apt-get install -y docker.io
-newgrp docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo usermod -a -G docker ubuntu
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install Singularity
-sudo apt-get install -y singularity
+
 
 # Install AWS CLI v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
