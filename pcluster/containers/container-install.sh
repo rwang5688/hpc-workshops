0d93812be96541720000ef5c9d292b21d628f793
Robert Wang <wangrob@amazon.com>
2023-09-08 08:04:14 +0000

    add singgularity installation

--- pcluster/containers/container-install.sh
+++ pcluster/containers/container-install.sh
@@ -6,10 +6,54 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install Singularity
+# https://docs.sylabs.io/guides/main/admin-guide/installation.html#id1
+# Ensure repositories are up-to-date
+sudo apt-get update
+# Install debian packages for dependencies
+sudo apt-get install -y \
+   build-essential \
+   libseccomp-dev \
+   libglib2.0-dev \
+   pkg-config \
+   squashfs-tools \
+   cryptsetup \
+   runc
+    
+# https://github.com/containers/conmon
+# [wangrob]: do below ONLY if Singularity fails to compile due to missing headers for conmon
+sudo apt-get install -y \
+    gcc \
+    git \
+    libc6-dev \
+    libglib2.0-dev \
+    libseccomp-dev \
+    pkg-config \
+    make \
+    runc
 
+export VERSION=1.21.1 OS=linux ARCH=amd64 && \
+  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+  rm go$VERSION.$OS-$ARCH.tar.gz
+  
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+  source ~/.bashrc
+
+# https://github.com/sylabs/singularity/releases/download/v3.11.4/singularity-ce-3.11.4.tar.gz  
+export VERSION=3.11.4 && \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz && \
+    tar -xzf singularity-ce-${VERSION}.tar.gz && \
+    cd singularity-ce-${VERSION}
+
+git clone --recurse-submodules https://github.com/sylabs/singularity.git && \
+    cd singularity && \
+    git checkout --recurse-submodules v${VERSION}
+
+./mconfig && \
+    make -C builddir && \
+    sudo make -C builddir install
 
 # Install AWS CLI v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-exit
